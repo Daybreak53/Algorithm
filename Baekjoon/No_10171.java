dbdad80edca62ee13ebd38e8a1d294cf739ec98d
Daybreak53 <wnghddk123@naver.com>
2022-05-24 00:29:38 +0900

    문제 풀이 추가

---
+++ Baekjoon/No_10171.java
@@ -0,0 +1,10 @@
+
+public class No_10171 {
+    public static void main (String[] args){
+        String s = "\\    /\\\n"
+            +" )  ( ')\n"
+            +"(  /  )\n"
+            +" \\(__)|\n";
+        System.out.print(s);
+    }
+}
